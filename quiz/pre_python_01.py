5a78be5232d44682c9e619c8e4f26499bdee92d3
kjm4967 <kjm4967@naver.com>
2020-06-11 15:56:12 +0900

    Answer quiz 01

--- quiz/pre_python_01.py
+++ quiz/pre_python_01.py
@@ -13,3 +13,16 @@
 ★★★★★
 ★★★★★
  """
+
+x = int(input("가로의 숫자를 입력하시오 :"))
+y = int(input("세로의 숫자를 입력하시오 :"))
+
+for j in range(y):
+    for i in range(x):
+        print("★", end='')
+    print()
+
+
+
+
+
